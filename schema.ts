d266ab85a17144610899af19a16a01d12aee2d75
Kunal Gala <kunal.gala16@gmail.com>
2024-06-13 23:08:21 -0400

    feat: added Technology Skills Banner schema and populated with data

--- schema.ts
+++ schema.ts
@@ -1,1 +1,35 @@
-export const schemaTypes = []
+export const schemaTypes = [
+  {
+    title: "Technology Skills Banner",
+    name: "technologySkillsBanner",
+    type: "document",
+    fields: [
+      { title: "Name", name: "name", type: "string" },
+      {
+        title: "List Items",
+        name: "listItems",
+        type: "array",
+        of: [
+          {
+            type: "object",
+            fields: [
+              {
+                title: "Image",
+                name: "image",
+                type: "image",
+                options: {
+                  hotspot: true,
+                },
+              },
+              {
+                title: "Name",
+                name: "name",
+                type: "string",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
